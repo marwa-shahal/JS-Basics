e5e646107200baf3efd5d527d309804f61726dc7
marwa <marwa.al.shahal@gmail.com>
2022-12-07 15:41:09 +0200

    step_6 step_5

--- javascript-basics-01/Step_6/Step_6.js
+++ javascript-basics-01/Step_6/Step_6.js
@@ -1,4 +1,12 @@
-let identity = document.getElementsByTagName("form")[0]
+const first_number_prompt= prompt("Enter your first number");
+let first_number_input = document.getElementById("first_number");
+first_number_input.value=first_number_prompt;
+
+const second_number_prompt= prompt("Enter your second_number");
+let second_number_input = document.getElementById("second_number");
+second_number.value=second_number_prompt;
+
+let identity = document.getElementsByTagName("form")[0];
 identity.addEventListener("submit", (e)=>{
     e.preventDefault()
     const first_number = e.target.first_number.value;
