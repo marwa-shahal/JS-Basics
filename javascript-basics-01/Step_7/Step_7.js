bdf9b6374e71eb20543df4a5af4a3cf97f1768da
marwa <marwa.al.shahal@gmail.com>
2022-12-07 15:29:30 +0200

    step_7

--- javascript-basics-01/Step_7/Step_7.js
+++ javascript-basics-01/Step_7/Step_7.js
@@ -1,9 +1,21 @@
+const shoe_size_prompt= prompt("Enter your shoe_size")
+let shoe_size_input = document.getElementById("shoe_size")
+shoe_size_input.value=shoe_size_prompt
+
+const age_prompt= prompt("Enter your age")
+let year_input = document.getElementById("year")
+year_input.value=age_prompt
+
 let identity = document.getElementsByTagName("form")[0]
 identity.addEventListener("submit", (e)=>{
     e.preventDefault()
     const shoe_size= e.target.shoe_size.value;
     const year= e.target.year.value;
-    let ans = (shoe_size*2+5)*50-year+1766
+    let ans = equ(shoe_size,year)
     alert(ans)
     identity.reset()
 })
+
+function equ(shoe_size,year){
+    return (shoe_size*2+5)*50-year+1766
+}
