ff864cf50fd7904b90fe76dff2b437c78e10e70f
marwa <marwa.al.shahal@gmail.com>
2022-12-07 16:49:59 +0200

    step_8 completed

--- javascript-basics-01/Step_8/Step_8.js
+++ javascript-basics-01/Step_8/Step_8.js
@@ -2,8 +2,18 @@ const age = prompt("Enter your age")
 let age_input = document.getElementById("age")
 age_input.value=age
 let validate = document.getElementById("validate")
-validate.addEventListener("click", ()=>{
-    const age=document.getElementById("age")
+// validate.addEventListener("click", ()=>{
+//     const age=document.getElementById("age")
+//     let ans=""
+//     if (age>18){
+//         ans="you are over 18"
+//     }else{
+//         ans="you are under 18"
+//     }
+//     alert(ans)
+// })
+validate.onclick=new_func
+function new_func(){
     let ans=""
     if (age>18){
         ans="you are over 18"
@@ -11,5 +21,4 @@ validate.addEventListener("click", ()=>{
         ans="you are under 18"
     }
     alert(ans)
-})
-
+}
